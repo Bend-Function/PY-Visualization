1002d2757f49cbe2573d38b596ff6ff712a88af1
Bend Function <42468146+Bend-Function@users.noreply.github.com>
2020-05-06 23:10:08 +0800

    Update com_bak.py

--- src/bilibili/com_bak.py
+++ src/bilibili/com_bak.py
@@ -6,25 +6,8 @@ import json
 
 # B站API详情 https://github.com/Vespa314/bilibili-api/blob/master/api.md
 
-# 视频AV号列表
-aid_list = []
-
-# 评论用户及其信息
+# 评论信息
 info_list = []
-
-# 获取指定UP的所有视频的AV号 mid:用户编号 size:单次拉取数目 page:页数
-def getAllAVList(mid, size, page):
-    # 获取UP主视频列表
-    for n in range(1,page+1):
-        url = "http://space.bilibili.com/ajax/member/getSubmitVideos?mid=" + \
-            str(mid) + "&pagesize=" + str(size) + "&page=" + str(n)
-        r = requests.get(url)
-        text = r.text
-        json_text = json.loads(text)
-        # 遍历JSON格式信息，获取视频aid
-        for item in json_text["data"]["vlist"]:
-            aid_list.append(item["aid"])
-    print(aid_list)
 
 # 获取一个AV号视频下所有评论
 def getAllCommentList(item):
@@ -53,12 +36,6 @@ def saveTxt(filename,filecontent):
         print("文件写入中")
 
 if __name__ == "__main__":
-    # 爬取逆风笑 只爬取第一页的第一个
-    # getAllAVList(2019740,1,1)
-    # for item in aid_list:
-    #     info_list.clear()
-    #     getAllCommentList(item)
-    #     saveTxt(item, info_list)
     aid_list = [412935552]
     for item in aid_list:        
         getAllCommentList(item)
